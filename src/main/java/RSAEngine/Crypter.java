077c8e56d2e320625fb0e7a519780ae39f9fc26d
Chetan Surpur <chetan51@gmail.com>
2010-09-21 13:38:48 -0700

    Finished Crypter's EncryptInteger and DecryptInteger methods

--- src/main/java/RSAEngine/Crypter.java
+++ src/main/java/RSAEngine/Crypter.java
@@ -1,5 +1,13 @@
 package main.java.RSAEngine;
 
-public class Crypter {
+import java.math.BigInteger;
 
+public class Crypter {
+	public BigInteger EncryptInteger(BigInteger m, BigInteger e, BigInteger n) {
+		return m.modPow(e, n);
+	}
+
+	public BigInteger DecryptInteger(BigInteger c, BigInteger d, BigInteger n) {
+		return c.modPow(d, n);
+	}
 }
--- src/test/java/RSAEngine/TestCrypter.java
+++ src/test/java/RSAEngine/TestCrypter.java
@@ -1,13 +1,32 @@
 package test.java.RSAEngine;
 
 import junit.framework.TestCase;
-
+import java.math.BigInteger;
 import main.java.RSAEngine.*;
 
 public class TestCrypter extends TestCase {
 
-    public void testHello()
+    public void testEncryptInteger_with_m_5_e_13_n_667()
     {
-        assertEquals("hello, world", "hello, world");
+		Crypter c = new Crypter();
+        assertEquals(c.EncryptInteger(BigInteger.valueOf(5), BigInteger.valueOf(13), BigInteger.valueOf(667)), BigInteger.valueOf(412));
+    }
+    
+	public void testEncryptInteger_with_m_7_e_13_n_667()
+    {
+		Crypter c = new Crypter();
+        assertEquals(c.EncryptInteger(BigInteger.valueOf(7), BigInteger.valueOf(13), BigInteger.valueOf(667)), BigInteger.valueOf(112));
+    }
+    
+	public void testDecryptInteger_with_c_412_d_237_n_667()
+    {
+		Crypter c = new Crypter();
+        assertEquals(c.DecryptInteger(BigInteger.valueOf(412), BigInteger.valueOf(237), BigInteger.valueOf(667)), BigInteger.valueOf(5));
+    }
+    
+	public void testDecryptInteger_with_m_112_d_237_n_667()
+    {
+		Crypter c = new Crypter();
+        assertEquals(c.DecryptInteger(BigInteger.valueOf(112), BigInteger.valueOf(237), BigInteger.valueOf(667)), BigInteger.valueOf(7));
     }
 }
