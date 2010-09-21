610b53290656287ef7dc080b96e20882977fe280
Chetan Surpur <chetan51@gmail.com>
2010-09-20 20:24:48 -0700

    Added project structure

---
+++ src/main/java/RSAEngine/Crypter.java
@@ -0,0 +1,5 @@
+package main.java.RSAEngine;
+
+public class Crypter {
+
+}
---
+++ src/test/java/RSAEngine/TestCrypter.java
@@ -0,0 +1,13 @@
+package test.java.RSAEngine;
+
+import junit.framework.TestCase;
+
+import main.java.RSAEngine.*;
+
+public class TestCrypter extends TestCase {
+
+    public void testHello()
+    {
+        assertEquals("hello, world", "hello, world");
+    }
+}
