8ff568991cd33e83594e5d5a5ccf1fb184977172
Chetan Surpur <chetan51@gmail.com>
2010-10-19 20:31:36 -0700

    Added comments to Signature

--- src/main/java/CryptoEngine/Signature.java
+++ src/main/java/CryptoEngine/Signature.java
@@ -15,32 +15,33 @@ public class Signature {
 	Crypter a;
 	
 	public Signature() throws NoSuchAlgorithmException{
+		//initialize my objects
 		SHA256 = MessageDigest.getInstance("SHA-256");
 		a = new Crypter();
 	}
 	
 	public String generateSignature(String username, String message, BigInteger d, BigInteger n) throws JSONException{
-		byte[] h = SHA256.digest(message.getBytes());
+		byte[] h = SHA256.digest(message.getBytes());				//get me a hash of message
 		BigInteger hash = new BigInteger(h);
-		BigInteger cypherhash = a.RSAEncryptPrimitive(hash, d, n);
-		JSONObject signature = new JSONObject();
+		BigInteger cypherhash = a.RSAEncryptPrimitive(hash, d, n);	//Encrypt my hash
+		JSONObject signature = new JSONObject();					//Initialize the JSON object and shove everything there
 		signature.put("username", username);
 		signature.put("message", message);
 		signature.put("signature", cypherhash);
-		return signature.toString();
+		return signature.toString();								//return the string
 	}
 	
 	public boolean verifySignature(String signature, BigInteger e, BigInteger n) throws JSONException{
-		JSONObject sig = new JSONObject(signature);
-		String message = sig.getString("message");
-		byte[] h = SHA256.digest(message.getBytes());
+		JSONObject sig = new JSONObject(signature);					//Build the JSON
+		String message = sig.getString("message");					//Pull out the message
+		byte[] h = SHA256.digest(message.getBytes());				//hash it
 		BigInteger hash = new BigInteger(h);
-		String cypherhash = (String) sig.getString("signature");
+		String cypherhash = (String) sig.getString("signature");	//get out the cyphertext
 		BigInteger c = new BigInteger(cypherhash);
-		BigInteger plainsig = a.RSADecryptPrimitive(c, e, n);
+		BigInteger plainsig = a.RSADecryptPrimitive(c, e, n);		//decrypt it
 		if(hash.compareTo(BigInteger.ZERO) < 0){
 			return plainsig.subtract(n).equals(hash);
 		}
-		return plainsig.equals(hash);
+		return plainsig.equals(hash);								//check validity
 	}
 }
