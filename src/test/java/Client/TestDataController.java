0d598e3e342419c1e0ae2582f95ef1feb7155c89
Chetan Surpur <chetan51@gmail.com>
2010-11-06 15:47:45 -0700

    Finished testing init function for data controller

--- src/test/java/Client/TestDataController.java
+++ src/test/java/Client/TestDataController.java
@@ -5,6 +5,11 @@ import java.math.BigInteger;
 
 import main.java.Client.*;
 
+/*
+ *  Note: The database needs to be set up as specified in the
+ *  README for these tests to properly work.
+ */
+
 public class TestDataController extends TestCase {
 
     DataController d;
@@ -13,12 +18,30 @@ public class TestDataController extends TestCase {
         d = new DataController();
     }
 
-    public void testInitialize() {
+    public void testInitializeWithInvalidUser() {
+        boolean testPassed = true;
+
         try {
-            d.initialize("test", "fails");
+            d.initialize("nouser", "testfails");
         }
         catch (Throwable e) {
+            testPassed = false;
         }
+
+        assertEquals(testPassed, false);
+    }
+
+    public void testInitializeWithValidUser() {
+        boolean testPassed = true;
+
+        try {
+            d.initialize("testuser", "test");
+        }
+        catch (Throwable e) {
+            testPassed = false;
+        }
+
+        assertEquals(testPassed, true);
     }
 
 }
