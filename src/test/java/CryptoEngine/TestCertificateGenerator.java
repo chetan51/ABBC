fe666c2525363dbdbbaafa697dd2fa01ff3a40e1
Chetan Surpur <chetan51@gmail.com>
2010-11-07 00:52:30 -0700

    Revert "Edited DataController"
    This reverts commit b2935126e6d54902b4d98fcbf15f9e48c172fabe.

---
+++ src/test/java/CryptoEngine/TestCertificateGenerator.java
@@ -0,0 +1,18 @@
+package test.java.CryptoEngine;
+
+import junit.framework.TestCase;
+import java.math.BigInteger;
+import main.java.CryptoEngine.*;
+import java.security.NoSuchAlgorithmException;
+
+import org.json.JSONException;
+
+public class TestCertificateGenerator extends TestCase {
+
+    public void setUp() {
+    }
+
+    public void testGenerate() {
+    }
+
+}
